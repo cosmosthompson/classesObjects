ec98298e466d9f074e2f9f517d68ef39ad067be0
Cosmos Thompson <cosmos.thompson@kudabank.com>
2022-05-28 22:17:03 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+    
+    def change_name(self, newname):
+        self.name = newname
+        print('My name is', self.name)
+
+    def change_age(self, newage):
+        self.age = newage
+        print('And my age is', self.age)
+
+    def add_track(self, tracks):
+        self.tracks.append(tracks)
+        print('I am offering tracks', self.tracks)
+
+    def get_score(self):
+        print('The score I got is', self.score)
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
